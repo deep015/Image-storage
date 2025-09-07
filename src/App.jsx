1086118b1385453a0be5c0cf8309f37fc25610e1
deepcodein <omraj767956@gmail.com>
2025-09-07 17:41:51 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import 'animate.css'
+const App = () => {
+  return (
+    <div className='bg-gray-100 min-h-screen'>
+      <div className='w-9/12 mx-auto py-10 space-y-8'>
+      <h1 className='text-3xl text-center font-bold text-gray-500'>Image Uploader</h1>
+      <div className='p-24 bg-red-500 rounded-xl '>
+
+      </div>
+      </div>
+      </div>
+  )
+}
+
+export default App
